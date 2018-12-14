d213ee92e442a16e4fe37c7746f0e0d6df575a8a
Ezequiel O. Fogliatto <ezefogliatto@gmail.com>
2018-12-14 10:59:37 -0300

    Ejecucion para cantidad fija de clientes, pero se elige aleatoriamente si son Publishers o Subscriptors

--- MQTTSim.cpp
+++ MQTTSim.cpp
@@ -1,15 +1,15 @@
 #include <iostream>
 
-#include "Broker.hpp"
-
 #include "SimPublisher.hpp"
 
 #include "SimSubscriptor.hpp"
 
-#include <vector>
+#include <random>
 
 
-// using namespace std;
+
+using namespace std;
+
 
 
 int main( int argc, char **argv ) {
@@ -19,52 +19,113 @@ int main( int argc, char **argv ) {
 
     Broker broker;
 
-
-    // Publisher threads
-
-    int npub = 1;
-
-    std::vector<SimPublisher*> publishers;
-
-    std::vector<std::thread> tpub;    
-
-    for( int i = 0 ; i < npub ; i++ )
-	publishers.push_back( new SimPublisher(broker, "Pub_" + std::to_string(i)) );
-
-    for( int i = 0 ; i < npub ; i++ )
-    	tpub.push_back( std::thread(&SimClient::start, publishers[i]) );
-
-
     
-    // Publisher threads
-
-    int nsub = 1;
-
-    std::vector<SimSubscriptor*> subscriptors;
-
-    std::vector<std::thread> tsub;    
-
-    for( int i = 0 ; i < nsub ; i++ )
-	subscriptors.push_back( new SimSubscriptor(broker, "Sub_" + std::to_string(i)) );
-
-    for( int i = 0 ; i < nsub ; i++ )
-    	tsub.push_back( std::thread(&SimClient::start, subscriptors[i]) );
-
-
-
     
+    // Clientes aleatorios
+    // Numero total por linea de comandos. Por defecto es 4
+    
+    auto gen = std::bind(std::uniform_int_distribution<>(0,1),std::default_random_engine());    
 
-    // Join publishers
+    int nclients(4),
+    	npub(0),
+    	nsub(0);
+    
+    if(argc == 2)
+    	nclients = std::atoi(argv[1]);
 
-    for(auto &th : tpub)
-    	if(th.joinable())
-    	    th.join();
 
 
-    // Join subscriptors
+    // Creacion de los clientes
+    
+    std::vector<SimClient*> Clients;
 
-    for(auto &th : tsub)
+    std::vector<std::thread> thclient;    
+
+    for( int i = 0 ; i < nclients ; i++ ) {
+
+    	if( gen() ) {
+	    
+    	    Clients.push_back( new SimPublisher(broker, "Pub_" + std::to_string(npub)) );
+
+    	    npub++;
+
+    	}
+
+    	else {
+
+    	    Clients.push_back( new SimSubscriptor(broker, "Sub_" + std::to_string(nsub)) );
+
+    	    nsub++;
+
+    	}
+
+
+    	// Comienzo de la simulacion para el cliente
+
+    	thclient.push_back( std::thread(&SimClient::start, Clients[i]) );	
+
+    }
+
+
+
+
+    // Join clients
+
+    for(auto &th : thclient)
     	if(th.joinable())
     	    th.join();    
+
+
+
+    
+
+
+
+    
+
+    // // Publisher threads
+
+    // int npub = 2;
+
+    // std::vector<SimPublisher*> publishers;
+
+    // std::vector<std::thread> tpub;    
+
+    // for( int i = 0 ; i < npub ; i++ )
+    // 	publishers.push_back( new SimPublisher(broker, "Pub_" + std::to_string(i)) );
+
+    // for( int i = 0 ; i < npub ; i++ )
+    // 	tpub.push_back( std::thread(&SimClient::start, publishers[i]) );
+
+
+    
+    // // Subscriptor threads
+
+    // int nsub = 2;
+
+    // std::vector<SimSubscriptor*> subscriptors;
+
+    // std::vector<std::thread> tsub;    
+
+    // for( int i = 0 ; i < nsub ; i++ )
+    // 	subscriptors.push_back( new SimSubscriptor(broker, "Sub_" + std::to_string(i)) );
+
+    // for( int i = 0 ; i < nsub ; i++ )
+    // 	tsub.push_back( std::thread(&SimClient::start, subscriptors[i]) );
+
+    
+
+    // // Join publishers
+
+    // for(auto &th : tpub)
+    // 	if(th.joinable())
+    // 	    th.join();
+
+
+    // // Join subscriptors
+
+    // for(auto &th : tsub)
+    // 	if(th.joinable())
+    // 	    th.join();    
         
 }
